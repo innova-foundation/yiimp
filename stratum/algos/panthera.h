abf1a0b6b0a7476d5b95f6c3d0324c689fd38f6a
0xcircuitbreaker <sarris88p@gmail.com>
2026-03-23 06:20:38 -0400

    add panthera algorithm support

---
+++ stratum/algos/panthera.h
@@ -0,0 +1,11 @@
+#ifndef PANTHERA_H
+#define PANTHERA_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include <stdint.h>
+void panthera_hash(const char* input, char* output, uint32_t len);
+#ifdef __cplusplus
+}
+#endif
+#endif
