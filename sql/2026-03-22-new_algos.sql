e2272addd0854e7096afbf93e6eaa15c081bf607
0xcircuitbreaker <sarris88p@gmail.com>
2026-03-23 06:20:45 -0400

    add sql migration for new algorithms

---
+++ sql/2026-03-22-new_algos.sql
@@ -0,0 +1,9 @@
+-- New algorithm support: KawPow, FiroPoW, ProgPoW, RandomX, Panthera, Equihash
+-- mysql yaamp -p < file.sql
+
+INSERT IGNORE INTO `algos` VALUES (39, 'kawpow', 0, 0, 1, 1);
+INSERT IGNORE INTO `algos` VALUES (40, 'firopow', 0, 0, 1, 1);
+INSERT IGNORE INTO `algos` VALUES (41, 'progpow', 0, 0, 1, 1);
+INSERT IGNORE INTO `algos` VALUES (42, 'randomx', 0, 0, 1, 1);
+INSERT IGNORE INTO `algos` VALUES (43, 'panthera', 0, 0, 1, 1);
+INSERT IGNORE INTO `algos` VALUES (44, 'equihash', 0, 0, 1, 1);
